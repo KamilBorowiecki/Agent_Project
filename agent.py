d5c98845cd52e0c8f74cbc6d5d7f062346c6fd24
KamilBorowiecki <kamil.borowiecki@op.pl>
2025-12-27 18:54:07 +0100

    feat: new model and tool with stock prices

--- agent.py
+++ agent.py
@@ -8,8 +8,12 @@ from langchain_community.vectorstores import Chroma
 from langchain_community.document_loaders import PyPDFLoader
 from langchain_community.document_loaders import OnlinePDFLoader
 from langchain_classic.retrievers.multi_query import MultiQueryRetriever
+from langchain_google_genai import ChatGoogleGenerativeAI
 import streamlit as st
 import time
+import yfinance as yf
+from dotenv import load_dotenv
+from langchain.tools import tool 
 
 @st.cache_resource
 def load_vector_db():
@@ -52,6 +56,11 @@ def call_bielik(prompt, question, vector_db):
         temperature=0.1
     )
 
+    bielik_model_with_tools = bielik_model.bind_tools([stock_price], 
+    tool_choice="stock_price"
+    )
+
+
     QUERY_PROMPT = PromptTemplate(
         input_variables=["question"],
         template=prompt,
@@ -73,7 +82,7 @@ def call_bielik(prompt, question, vector_db):
 
     final_template = """odpowiedz na pytanie bazujac na ponizszym kontekstu:
     {context}
-    Podaj fragment pochodzący z kontekstu
+    Jeśli pytanie dotyczy aktualnej ceny akcji, kursu giełdowego lub wartości rynkowej, MUSISZ użyć narzędzia 'stock_price'. Nie zgaduj.
     Question: {question}
     """
 
@@ -83,12 +92,82 @@ def call_bielik(prompt, question, vector_db):
     chain = (
         {"context": retriever, "question": RunnablePassthrough()} 
         | chat_prompt
-        | bielik_model
-        | StrOutputParser()
+        | bielik_model_with_tools
     )
 
     response = chain.invoke({"question": question})
-    return response, None
+    if response.tool_calls:
+        return response, "TOOL_CALL"
+    else:
+        return response.content, None
+
+
+@safe_api_call
+def call_gemini(prompt, question, vector_db):
+    gemini_model = ChatGoogleGenerativeAI(
+        model="gemini-3-flash-preview", 
+        temperature=0.1
+    )
+
+    gemini_model_with_tools = gemini_model.bind_tools([stock_price], 
+    tool_choice="stock_price"
+    )
+
+    QUERY_PROMPT = PromptTemplate(
+        input_variables=["question"],
+        template=prompt,
+    )
+    
+    try:
+        if vector_db:
+            retriever = MultiQueryRetriever.from_llm(
+                vector_db.as_retriever(),
+                gemini_model,
+                prompt=QUERY_PROMPT
+            )
+        else:
+            retriever = None
+    except Exception as e:
+        print("Błąd MultiQueryRetriever:")
+        print(e)
+        retriever = None
+
+    final_template = """odpowiedz na pytanie bazujac na ponizszym kontekstu:
+    {context}
+    Jeśli pytanie dotyczy aktualnej ceny akcji, kursu giełdowego lub wartości rynkowej, MUSISZ użyć narzędzia 'stock_price'. Nie zgaduj.
+    Question: {question}
+    """
+
+
+    chat_prompt = ChatPromptTemplate.from_template(final_template)
+
+    chain = (
+        {"context": retriever, "question": RunnablePassthrough()} 
+        | chat_prompt
+        | gemini_model_with_tools
+    )
+
+    response = chain.invoke({"question": question})
+    if response.tool_calls:
+        return response, "TOOL_CALL"
+    else:
+        return response.content, None
+
+@tool
+def stock_price(ticker: str) -> str:
+    """
+    Pobiera aktualną cenę akcji dla danego tickera (np. AAPL, TSLA).
+    """
+
+    stock = yf.Ticker(ticker)
+    price = stock.info['regularMarketPrice']
+    print("wykorzystalem toola")
+
+    if price is None:
+        return "Nie udało się pobrać ceny."
+
+    return f"Aktualna cena {ticker.upper()} wynosi {price} USD"
+    
 
 # PROMPT = """
 #         Jestes Polskim asystenem inwestowania, 
@@ -104,48 +183,71 @@ PROMPT = """You are an AI language model assistant. Your task is to generate fiv
     similarity search. Provide these alternative questions separated by newlines.
     Original question: {question}"""
 
-def runStreamlit():
-    st.title("LLM Model Comparison")
+def run_streamlit():
+    load_dotenv()
+
+    st.set_page_config(page_title="LLM Compare", layout="wide")
+    st.title("🤖 Porównanie modeli LLM")
 
     vector_db = load_vector_db()
 
-
-    tests = [
+    models = [
         ("Bielik", call_bielik),
+        ("Gemini", call_gemini),
     ]
-
-    prompts = {
-        "text": PROMPT
-    }
 
     if "history" not in st.session_state:
         st.session_state.history = []
 
-    question = st.text_input("Zadaj pytanie do modelu:")
+    question = st.text_input("Zadaj pytanie:")
 
     if st.button("▶️ Wyślij") and question:
         responses = []
-        for name, func in tests:
-            for nameP, prompt in prompts.items():
-                with st.spinner(f"Wywołanie: {name}..."):
-                    start = time.time()
-                    text, error = func(prompt, question, vector_db)
-                    elapsed = time.time() - start
-                    responses.append((name, text, elapsed, error))
+
+        for name, func in models:
+            with st.spinner(f"{name} odpowiada..."):
+                start = time.time()
+                res, status = func(PROMPT, question, vector_db)
+                
+                if status == "TOOL_CALL":
+                    tool_call = res.tool_calls[0]
+                    args = tool_call["args"]
+                    
+                    try:
+                        result = stock_price.invoke(args)
+                        display_text = result 
+                    except Exception as e:
+                        display_text = f"Błąd podczas pobierania ceny: {e}"
+                else:
+                    display_text = res
+
+                elapsed = time.time() - start
+                
+                responses.append((name, display_text, elapsed, None))
 
         st.session_state.history.append((question, responses))
 
     if st.session_state.history:
-        st.header("Historia pytań i odpowiedzi")
-        for i, (q, res_list) in enumerate(st.session_state.history[::-1]):
-            st.subheader(f"Q{i+1}: {q}")
-            for name, text, elapsed, error in res_list:
-                st.markdown(f"**Model {name}** ({elapsed:.2f}s):")
-                if error:
-                    st.error(error)
-                else:
-                    st.info(text)
+        st.header("📜 Historia")
+
+        for q, results in reversed(st.session_state.history):
+            st.subheader(f"❓ {q}")
+
+            cols = st.columns(len(results))
+
+            for col, (name, text, elapsed, error) in zip(cols, results):
+                with col:
+                    st.markdown(f"### {name}")
+                    st.caption(f"⏱ {elapsed:.2f}s")
+
+                    if error:
+                        st.error(error)
+                    else:
+                        st.chat_message("assistant").markdown(text)
+
             st.markdown("---")
 
+
 if __name__ == "__main__":
-    runStreamlit()
+    run_streamlit()
+
